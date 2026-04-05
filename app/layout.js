67a28026e12a232f11e3224355cc5700898f0fde
AmericanAlliance <americanalliance2026@proton.me>
2026-04-04 22:54:18 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
 export const metadata = {
   title: "American Alliance",
-  description: "A movement to unify disenfranchised voters from the right, left, and middle around the big issues."
+  description:
+    "A movement to unify disenfranchised voters from the right, left, and middle around the big issues.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
